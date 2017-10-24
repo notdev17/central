13a1d068e1f24b38f92efd79ab7e6b4660368c94
Tim <timonyang@gmail.com>
2017-10-24 13:54:14 -0700

    "Added name"

--- EasterEgg/src/EasterNamePanel.java
+++ EasterEgg/src/EasterNamePanel.java
@@ -7,7 +7,7 @@ public class EasterNamePanel extends JPanel {
   // Add your name to this array
   // Just made an array so that JUnit test can just check for array length == 5
   // to see if everyone added their name. 
-  private static final String[] NAMES = {"Daylen", "Brandon", "Michelle"};
+  private static final String[] NAMES = {"Daylen", "Brandon", "Michelle", "Tim"};
 
   public EasterNamePanel() {
     super();
