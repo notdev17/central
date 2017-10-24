80b1f137e48367fbd1d258f2a0bcec30d3166d59
michellesuchang <michellesuchang@gmail.com>
2017-10-24 11:45:24 -0700

    Added New Class
    Added an EasterNamePanel class.

--- EasterEgg/src/EasterFrame.java
+++ EasterEgg/src/EasterFrame.java
@@ -9,7 +9,7 @@ import javax.swing.JPanel;
 public class EasterFrame extends JFrame {
 	//width and height of the frame
 	private static final int SIZE = 500;
-	
+
 	public EasterFrame () {
 		super("Easter Egg");
 
@@ -17,36 +17,37 @@ public class EasterFrame extends JFrame {
 		this.setLayout(new BorderLayout());
 		this.setPreferredSize(new Dimension(SIZE, SIZE));
 		this.setSize(this.getPreferredSize());
-		
+
 		//Centering the JFrame
 		this.pack();
 		this.setLocationRelativeTo(null);
-		
+
 		//create and add button and panel to the frame
 		addContents();
 	}
-	
+
 	//creates and adds the panel and button to the frame
 	private void addContents() {
-		
+
 		//create the panel & set layout
 		JPanel panel = new JPanel();
 		panel.setLayout(new GridBagLayout());
-		
+
 		//create the button & set size
 		JButton butt  = new JButton("We are...");
 		butt.setPreferredSize(new Dimension(this.getWidth()/3, this.getHeight()/4));
-		
+
+		JPanel names = new EasterNamePanel();
+
 		//create the action listener for the button
 		butt.addActionListener(event -> {
-			//show a message dialogue displaying members (ADD YOUR NAME HERE with (\n) for new lines)
-			JOptionPane.showMessageDialog(this, "Daylen\nBrandon", "Not Dev!",JOptionPane.INFORMATION_MESSAGE);
+			JOptionPane.showMessageDialog(this, names, "Not Dev!",JOptionPane.INFORMATION_MESSAGE);
 		});
-		
+
 		//add the panel and button to the frame
 		panel.add(butt);
 		this.add(panel);
 	}
-	
-	
+
+
 }
---
+++ EasterEgg/src/EasterNamePanel.java
@@ -0,0 +1,23 @@
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import java.awt.GridLayout;
+
+public class EasterNamePanel extends JPanel {
+
+  // Add your name to this array
+  // Just made an array so that JUnit test can just check for array length == 5
+  // to see if everyone added their name. 
+  private static final String[] NAMES = {"Daylen", "Brandon", "Michelle"};
+
+  public EasterNamePanel() {
+    super();
+    setup();
+  }
+
+  public void setup(){
+    this.setLayout(new GridLayout(0,1));
+    for(int i = 0; i < NAMES.length; i++) {
+      this.add(new JLabel(NAMES[i]));
+    }
+  }
+}
