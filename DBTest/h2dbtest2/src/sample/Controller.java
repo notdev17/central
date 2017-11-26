a508881c5d2ca252d31e15b90bb8ec9149451c03
Tim <timyang@uw.edu>
2017-11-26 05:21:42 -0800

    Add files via upload

---
+++ DBTest/h2dbtest2/src/sample/Controller.java
@@ -0,0 +1,68 @@
+package sample;
+
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.util.Callback;
+
+import java.awt.event.KeyEvent;
+import java.sql.ResultSet;
+
+import static sample.Main.db;
+
+public class Controller{
+    @FXML
+    private TableView tableview;
+    private ObservableList<ObservableList> data;
+
+    @FXML
+    public void populateTable(ActionEvent event) {
+
+        data = FXCollections.observableArrayList();
+
+        try {
+            db.sql("SELECT * FROM TEST");
+            ResultSet rs = db.getResult();
+            for(int i=0 ; i<rs.getMetaData().getColumnCount(); i++){
+                final int j = i;
+                TableColumn col = new TableColumn(rs.getMetaData().getColumnName(i+1));
+                col.setCellValueFactory(new Callback<TableColumn.CellDataFeatures<ObservableList,String>,ObservableValue<String>>(){
+                    public ObservableValue<String> call(TableColumn.CellDataFeatures<ObservableList, String> param) {
+                        return new SimpleStringProperty(param.getValue().get(j).toString());
+                    }
+                });
+
+                tableview.getColumns().addAll(col);
+            }
+
+
+            /********************************
+             * Data added to ObservableList *
+             ********************************/
+            while(rs.next()){
+                //Iterate Row
+                ObservableList<String> row = FXCollections.observableArrayList();
+                for(int i=1 ; i<=rs.getMetaData().getColumnCount(); i++){
+                    //Iterate Column
+                    row.add(rs.getString(i));
+                }
+                System.out.println("Row [1] added "+row );
+                data.add(row);
+
+            }
+
+            //FINALLY ADDED TO TableView
+            tableview.setItems(data);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
---
+++ DBTest/h2dbtest2/src/sample/Main.java
@@ -0,0 +1,32 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
+
+public class Main extends Application {
+
+    public static H2Database db = new H2Database();
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+
+        db.command("DROP ALL OBJECTS");
+        db.command("CREATE TABLE TEST AS SELECT * FROM CSVREAD('lighting.csv');");
+
+
+        launch(args);
+    }
+}
